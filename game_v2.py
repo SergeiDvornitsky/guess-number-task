2f8824a53f97b352cde48c7669b7d1ccb3cdbf67
SergeiDvonitsky <trio271@mail.ru>
2022-12-07 17:12:00 +0300

    Initial commit

--- game_v2.py
+++ game_v2.py
@@ -16,11 +16,13 @@ def random_predict(number: int = 1) -> int:
     """
     count = 0
 
-    while True:
+    while count < 20:
         count += 1
         predict_number = np.random.randint(1, 101)  # предполагаемое число
+        
         if number == predict_number:
             break  # выход из цикла если угадали
+    
     return count
 
 
